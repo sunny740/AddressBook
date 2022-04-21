de3a7dae3443025ea3b4a179b8591bf9b12149f8
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-21 23:20:15 +0530

    [Sunny] Add : Adding New Contact

---
+++ AddressBooks/AddressBook.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+
+namespace AddressBooks
+{
+    public class AddressBook
+    {
+        List<Contact> addContact = new List<Contact>();
+        Contact contact = new Contact();
+        public void AddContact()
+        {
+
+            Console.WriteLine("Enter the Address Details");
+
+            Contact contact1 = new Contact()
+            {
+                FirstName = Convert.ToString(Console.ReadLine()),
+
+                LastName = Convert.ToString(Console.ReadLine()),
+
+                Address = Convert.ToString(Console.ReadLine()),
+
+                City = Convert.ToString(Console.ReadLine()),
+
+                State = Convert.ToString(Console.ReadLine()),
+
+                ZipCode = Convert.ToInt32(Console.ReadLine()),
+
+                MobileNumber = Convert.ToInt64(Console.ReadLine()),
+
+                Email = Convert.ToString(Console.ReadLine()),
+
+            };
+            Contact contact2 = new Contact()
+            {
+                FirstName = "Sunny",
+
+                LastName = "Sejwar",
+
+                Address = "Gwalior",
+
+                City = "Gwalior",
+
+                State = "MP",
+
+                ZipCode = 490021,
+
+                MobileNumber = 78828883835,
+
+                Email = "Sunnysejwar@com",
+
+            };
+
+            Contact contact3 = new Contact()
+            {
+                FirstName = "Vansh",
+
+                LastName = "Sharma",
+
+                Address = "Bhopal",
+
+                City = "BHopal",
+
+                State = "MP",
+
+                ZipCode = 490055,
+
+                MobileNumber = 99828883835,
+
+                Email = "Vansh@gmail.com",
+
+            };
+            addContact.Add(contact1);
+            addContact.Add(contact2);
+            addContact.Add(contact3);
+        }
+        public void Display()
+        {
+            foreach (var contact in addContact)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Address + " " + contact.City + " " + contact.State + " " + contact.ZipCode + " " + contact.MobileNumber + " " + contact.Email);
+            }
+        }
+
+    }
+}
--- AddressBooks/Program.cs
+++ AddressBooks/Program.cs
@@ -1,36 +1,32 @@
 ﻿using System;
 using AddressBooks;
-
 class program
 {
     public static void Main(String[] args)
     {
-        AddressBook AB = new AddressBook();
-        AB.Book();
-        Console.WriteLine("Enter the Address Details");
-        AddressBooks.Contact contact = new Contact();
-        Console.WriteLine("Enter FirstName");
-        contact.FirstName = Convert.ToString(Console.ReadLine());
+        AddressBook contact = new AddressBook();
 
-        Console.WriteLine("Enter LastName");
-        contact.LastName = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Welcome to Address Book Project");
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("1.Add Contact\n2.Display Contact");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    contact.AddContact();
+                    break;
+                case 2:
+                    contact.Display();
+                    break;
+                case 0:
+                    check = false;
+                    break;
 
-        Console.WriteLine("Enter Address");
-        contact.Address = Convert.ToString(Console.ReadLine());
+            }
+        }
 
-        Console.WriteLine("Enter City");
-        contact.City = Convert.ToString(Console.ReadLine());
-
-        Console.WriteLine("Enter State");
-        contact.State = Convert.ToString(Console.ReadLine());
-        Console.WriteLine("Enter Email");
-
-        contact.Email = Convert.ToString(Console.ReadLine());
-
-        Console.WriteLine("Enter Zip");
-        contact.ZipCode = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter Phone Number");
-        contact.MobileNumber = Convert.ToInt64(Console.ReadLine());
     }
+
 }
